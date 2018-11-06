6dc7dca1ef543a5abc3aec9b1d03610150a264d1
Igor Davydychev <igor.davydychev@gmail.com>
2018-11-06 09:03:27 +0200

    Add basic harbors app
    Harbors app will store data for harbors and boat types.

--- berth_reservations/settings.py
+++ berth_reservations/settings.py
@@ -97,6 +97,7 @@ INSTALLED_APPS = [
 
     'reservations',
     'notifications',
+    'harbors',
 ]
 
 MIDDLEWARE = [
---
+++ harbors/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class HarborsConfig(AppConfig):
+    name = 'harbors'
