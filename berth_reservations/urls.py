2ccc81ac9faaa61acd98b624f618180a68f32a7c
Igor Davydychev <igor.davydychev@gmail.com>
2018-11-12 10:22:10 +0200

    Serve media files when DEBUG is on

--- berth_reservations/urls.py
+++ berth_reservations/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 from rest_framework import routers
@@ -17,3 +19,6 @@ urlpatterns = [
     path('v1/', include(router.urls)),
     path('docs/', include_docs_urls(title='Berth reservations')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
