82ff55b86c7cae8201c79acd36dd7adb58dfd159
Igor Davydychev <igor.davydychev@gmail.com>
2019-01-17 09:22:25 +0200

    Allow deleting reservations
    Deletion permission will be better managed with
    user permissions.

--- reservations/admin.py
+++ reservations/admin.py
@@ -23,14 +23,5 @@ class ReservationAdmin(admin.ModelAdmin):
 
     export_reservations.short_description = _("Download list of chosen reservations in CSV format")
 
-    def get_actions(self, request):
-        actions = super().get_actions(request)
-        if 'delete_selected' in actions:
-            del actions['delete_selected']
-        return actions
 
-    def has_delete_permission(self, request, obj=None):
-        return False
-
-    
 admin.site.register(Reservation, ReservationAdmin)
