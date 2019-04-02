7dd7ee3e093c3949e8cbf0175c4916e857a96c19
Igor Davydychev <igor.davydychev@gmail.com>
2019-04-02 15:10:39 +0300

    Add winter storage areas
    These areas are not directly related to harbor. They might be
    near a harbor, but not necessarily. They can have any combination
    of the following types of places, but most have only one type:
    
    1. Ruutupaikat (appointed marked places)
       - have certain dimensions (width x length)
       - can be reserved by individuals personally
       - number of places known
    
    2. Lohkopaikat (section places)
       - one big section can fit an approximate # of boats
       - gets filled up as long as there is still space left
       - people just reserve a place in the queue, but who gets
         a place and who does not, only becomes clear, when people
         start putting their boats there
       - section has a certain width, which limits the length of
         suitable boats, since they get placed side by side within
         these sections
    
    3. Nostojärjestyspaikat (unmarked places)
       - area with no marked sections or places
       - people just put their boats at random within reason
       - will eventually be transformed into lohkopaikat
       - same rules for queing as with lohkopaikat
       - no dimensions
       - very approximate number of places

--- harbors/models.py
+++ harbors/models.py
@@ -31,6 +31,12 @@ def get_harbor_media_folder(instance, filename):
     )
 
 
+def get_winter_area_media_folder(instance, filename):
+    return "winter_areas/{area_id}/{filename}".format(
+        area_id=instance.id, filename=filename
+    )
+
+
 class OverwriteStorage(FileSystemStorage):
     """
     Custom storage that deletes previous harbor images
@@ -69,7 +75,7 @@ class AvailabilityLevel(TranslatableModel):
         return self.safe_translation_getter("title", super().__str__())
 
 
-class Harbor(TranslatableModel):
+class AbstractArea(models.Model):
     # For importing coordinates and address from servicemap.hel.fi
     servicemap_id = models.CharField(
         verbose_name=_("Servicemap ID"),
@@ -96,6 +102,20 @@ class Harbor(TranslatableModel):
         verbose_name=_("Location"), blank=True, null=True, srid=settings.DEFAULT_SRID
     )
 
+    image_link = models.URLField(
+        verbose_name=_("Image link"), max_length=400, null=True, blank=True
+    )
+
+    # Common services
+    electricity = models.BooleanField(verbose_name=_("Electricity"), default=False)
+    water = models.BooleanField(verbose_name=_("Water"), default=False)
+    gate = models.BooleanField(verbose_name=_("Gate"), default=False)
+
+    class Meta:
+        abstract = True
+
+
+class Harbor(AbstractArea, TranslatableModel):
     municipality = models.ForeignKey(
         Municipality,
         null=True,
@@ -112,18 +132,12 @@ class Harbor(TranslatableModel):
         null=True,
         blank=True,
     )
-    image_link = models.URLField(
-        verbose_name=_("Image link"), max_length=400, null=True, blank=True
-    )
 
-    # Available services
+    # Additional harbor services
     mooring = models.BooleanField(verbose_name=_("Mooring"), default=False)
-    electricity = models.BooleanField(verbose_name=_("Electricity"), default=False)
-    water = models.BooleanField(verbose_name=_("Water"), default=False)
     waste_collection = models.BooleanField(
         verbose_name=_("Waste collection"), default=False
     )
-    gate = models.BooleanField(verbose_name=_("Gate"), default=False)
     lighting = models.BooleanField(verbose_name=_("Lighting"), default=False)
 
     suitable_boat_types = models.ManyToManyField(
@@ -174,6 +188,86 @@ class Harbor(TranslatableModel):
         verbose_name = _("harbor")
         verbose_name_plural = _("harbors")
         ordering = ("id",)
+
+    def __str__(self):
+        return self.safe_translation_getter("name", super().__str__())
+
+
+class WinterStorageArea(AbstractArea, TranslatableModel):
+    municipality = models.ForeignKey(
+        Municipality,
+        null=True,
+        blank=True,
+        verbose_name=_("Municipality"),
+        related_name="winter_storage_areas",
+        on_delete=models.SET_NULL,
+    )
+
+    image_file = models.ImageField(
+        upload_to=get_winter_area_media_folder,
+        storage=OverwriteStorage(),
+        verbose_name=_("Image file"),
+        null=True,
+        blank=True,
+    )
+
+    # Additional winter storage area services
+    repair_area = models.BooleanField(verbose_name=_("Repair area"), default=False)
+    summer_storage_for_docking_equipment = models.BooleanField(
+        verbose_name=_("Summer storage for docking equipment"), default=False
+    )
+    summer_storage_for_trailers = models.BooleanField(
+        verbose_name=_("Summer storage for trailers"), default=False
+    )
+    summer_storage_for_boats = models.BooleanField(
+        verbose_name=_("Summer storage for boats"), default=False
+    )
+
+    # Ruutupaikat (~ appointed marked places)
+    # We can see in advance who gets a place and who does not.
+    # We know their lengths and widths.
+    number_of_marked_places = models.PositiveSmallIntegerField(
+        verbose_name=_("Number of marked places"), null=True, blank=True
+    )
+    max_width = models.PositiveSmallIntegerField(
+        verbose_name=_("Maximum place width"), null=True, blank=True
+    )
+    max_length = models.PositiveSmallIntegerField(
+        verbose_name=_("Maximum place length"), null=True, blank=True
+    )
+
+    # Lohkopaikat (~ section places)
+    # People just queue for these, then as long as there s still space
+    # next person in the queue can put his/her boat there.
+    # The area is separated into sections, that limit the length of the suitable boat.
+    number_of_section_spaces = models.PositiveSmallIntegerField(
+        verbose_name=_("Number of section places"), null=True, blank=True
+    )
+    max_length_of_section_spaces = models.PositiveSmallIntegerField(
+        verbose_name=_("Maximum length of section spaces"), null=True, blank=True
+    )
+
+    # Nostojärjestyspaikat (~ unmarked places)
+    # Same queing algorithm as with lohkopaikat.
+    # No data of the dimensions.
+    number_of_unmarked_spaces = models.PositiveSmallIntegerField(
+        verbose_name=_("Number of unmarked places"), null=True, blank=True
+    )
+
+    translations = TranslatedFields(
+        name=models.CharField(
+            verbose_name=_("name"),
+            max_length=200,
+            help_text=_("Name of the area"),
+            blank=True,
+        ),
+        street_address=models.CharField(
+            verbose_name=_("street address"),
+            max_length=200,
+            help_text=_("Street address of the area"),
+            blank=True,
+        ),
+    )
 
     def __str__(self):
         return self.safe_translation_getter("name", super().__str__())
