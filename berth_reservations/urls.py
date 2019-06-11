99b00513531371150874c099c90e23f75090e914
Igor Davydychev <igor.davydychev@gmail.com>
2019-06-11 14:00:45 +0300

    Send graphql errors to sentry always
    By default the graphql errors in django-graphene are silenced.
    This makes for a difficult debugging experience, so let's send
    them explicitly to sentry whenever possible.

--- berth_reservations/urls.py
+++ berth_reservations/urls.py
@@ -3,11 +3,12 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 from django.views.decorators.csrf import csrf_exempt
-from graphene_django.views import GraphQLView
+
+from .views import SentryGraphQLView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("graphql/", csrf_exempt(GraphQLView.as_view(graphiql=True))),
+    path("graphql/", csrf_exempt(SentryGraphQLView.as_view(graphiql=True))),
 ]
 
 if settings.DEBUG:
