cc9854c0c425681e3efd3e8ddf92b0e33e187d12
Toni Leino <toni@leino.net>
2019-07-01 16:02:11 +0300

    Add check for berth switch reason

--- reservations/tests/test_reservations_graphql.py
+++ reservations/tests/test_reservations_graphql.py
@@ -89,6 +89,84 @@ def test_create_berth_reservation(boat_type, harbor, berth_switch_reason):
     }
 
 
+def test_create_berth_reservation_wo_reason(boat_type, harbor):
+    client = GraphQLTestClient()
+    t = Template(
+        """
+        mutation createBerthReservation {
+            createBerthReservation(
+                berthSwitch: {
+                    harborId: \"${current_harbor}\",
+                    pier: "dinkkypier",
+                    berthNumber: "D33",
+                },
+                berthReservation: {
+                    language: "en",
+                    firstName: "John",
+                    lastName: "Doe",
+                    phoneNumber: "1234567890",
+                    email: "john.doe@example.com",
+                    address: "Mannerheimintie 1",
+                    zipCode: "00100",
+                    municipality: "Helsinki",
+                    boatType: ${boat_type_id},
+                    boatWidth: 2,
+                    boatLength: 3,
+                    informationAccuracyConfirmed: true,
+                    acceptFitnessNews: false,
+                    acceptLibraryNews: false,
+                    acceptOtherCultureNews: false,
+                    acceptBoatingNewsletter: true,
+                    choices: [
+                        {
+                            harborId: \"${desired_harbor}\",
+                            priority: 1
+                        }
+                    ]
+                }
+            ) {
+                berthReservation {
+                    berthSwitch {
+                        berthNumber,
+                        reason {
+                            id
+                        }
+                    },
+                    chosenHarbors {
+                        edges {
+                            node {
+                                properties {
+                                   zipCode
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        """
+    )
+    harbor_node_id = to_global_id(HarborType._meta.name, harbor.id)
+    mutation = t.substitute(
+        current_harbor=harbor_node_id,
+        boat_type_id=boat_type.id,
+        desired_harbor=harbor_node_id,
+    )
+    executed = client.execute(mutation)
+    assert executed == {
+        "data": {
+            "createBerthReservation": {
+                "berthReservation": {
+                    "berthSwitch": {"berthNumber": "D33", "reason": None},
+                    "chosenHarbors": {
+                        "edges": [{"node": {"properties": {"zipCode": "00100"}}}]
+                    },
+                }
+            }
+        }
+    }
+
+
 def test_create_winter_storage_reservation(boat_type, winter_area):
     client = GraphQLTestClient()
     t = Template(
