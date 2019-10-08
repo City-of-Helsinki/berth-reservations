8509d8b16503a418017607c4a5f185ac1fc97941
Igor Davydychev <igor.davydychev@gmail.com>
2019-10-08 16:42:54 +0300

    Add tests for reservation notifications

--- berth_reservations/tests/conftest.py
+++ berth_reservations/tests/conftest.py
@@ -8,6 +8,12 @@ def autouse_django_db(db):
     pass
 
 
+@pytest.fixture(autouse=True)
+def force_settings(settings):
+    settings.MAILER_EMAIL_BACKEND = "django.core.mail.backends.locmem.EmailBackend"
+    settings.DEFAULT_FROM_EMAIL = "noreply@foo.bar"
+
+
 @pytest.fixture
 def admin_user():
     user = UserFactory()
