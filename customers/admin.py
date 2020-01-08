1b3d3fa2552d74a3d6c9109acf0df807e1790262
Igor Davydychev <igor.davydychev@gmail.com>
2020-01-08 14:43:13 +0200

    List boats in django admin

--- customers/admin.py
+++ customers/admin.py
@@ -1,9 +1,16 @@
 from django.contrib import admin
 
-from .models import CustomerProfile
+from .models import Boat, CustomerProfile
 
 
 class CustomerProfileInline(admin.StackedInline):
     model = CustomerProfile
     fk_name = "user"
     extra = 0
+
+
+class BoatAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Boat, BoatAdmin)
