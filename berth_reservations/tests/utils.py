116949cfff2efd29b80fc0e9cf7e6b5604893fad
Igor Davydychev <igor.davydychev@gmail.com>
2020-03-11 14:23:32 +0200

    Delete old GraphQLDjangoTestClient
    We used it to, because it allowed to pass Accept-Language header
    with the request, as it went through Django's LocaleMiddleware,
    which switches the language based in the header value.
    Now we use Graphene's test Client for tests and simply enforce
    English in our API calls. It is more flexible and would make
    certain tests easier (e.g. file upload tests).
    
    Django already has tests for LocaleMiddleware:
    https://github.com/django/django/blob/2.2.11/tests/i18n/tests.py#L1244
    so we can just skip testing that Accept-Language header works.

--- berth_reservations/tests/utils.py
+++ berth_reservations/tests/utils.py
@@ -1,7 +1,5 @@
-import json
-
 from django.contrib.auth.models import AnonymousUser
-from django.test import Client, RequestFactory
+from django.test import RequestFactory
 from graphene.test import Client as GrapheneClient
 
 from berth_reservations.new_schema import new_schema
@@ -45,65 +43,6 @@ def create_api_client(user=None, graphql_v2=True):
     client.user = user
 
     return client
-
-
-class GraphQLDjangoTestClient(Client):
-    """
-    Custom test client to allow configuring language header.
-    Standard graphene.test.Client does not allow doing that.
-    """
-
-    def execute(
-        self,
-        query="",
-        op_name=None,
-        variables=None,
-        graphql_url="/graphql/",
-        lang="en",
-        user=None,
-    ):
-        """
-        Function that posts the passed query to our project's GraphQL endpoint.
-
-        :param query: GraphQL query to run
-        :type query: str
-        :param op_name: If the query is a mutation or named query, you must
-                        supply the op_name.  For annon queries ("{ ... }"),
-                        should be None (default).
-        :type op_name: str
-        :param variables: If provided, the $input variable in GraphQL will be set
-                          to this value
-        :type variables: None | dict
-        :param lang: Language to be set in the "Accept-Language" header.
-        :type lang: str
-        :param user: In case the query requires authentication, a "user" with the
-                     desired permissions can be passed. It will only attempt to
-                     login if a user is passed.
-        :type user: users.models.User
-
-        :return: response from graphql endpoint.  The response has the "data" key.
-                 It will have the "error" key if any error happened.
-        :rtype: dict
-        """
-
-        body = {"query": query}
-        if op_name:
-            body["operation_name"] = op_name
-        if variables:
-            body["variables"] = {"input": variables}
-
-        if user:
-            self.force_login(user)
-
-        resp = self.post(
-            graphql_url,
-            json.dumps(body),
-            content_type="application/json",
-            HTTP_ACCEPT_LANGUAGE=lang,
-        )
-
-        response_json = json.loads(resp.content.decode())
-        return response_json
 
 
 def assert_not_enough_permissions(executed):
