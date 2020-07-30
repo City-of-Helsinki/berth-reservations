6c07592c4469e8e8e3032641ff031e187a682137
Alex Ramirez <armzprz@gmail.com>
2020-07-30 15:21:38 +0300

    Migrate Payments to Django enums

--- payments/enums.py
+++ payments/enums.py
@@ -1,33 +1,27 @@
+from django.db.models import TextChoices
 from django.utils.translation import gettext_lazy as _
-from enumfields import Enum
 
 
-class ProductServiceType(Enum):
+class ProductServiceType(TextChoices):
     # Fixed services
-    ELECTRICITY = "electricity"
-    WATER = "water"
-    GATE = "gate"
-    MOORING = "mooring"
-    WASTE_COLLECTION = "waste_collection"
-    LIGHTING = "lighting"
+    ELECTRICITY = "electricity", _("Electricity")
+    WATER = "water", _("Water")
+    GATE = "gate", _("Gate")
+    MOORING = "mooring", _("Mooring")
+    WASTE_COLLECTION = "waste_collection", _("Waste collection")
+    LIGHTING = "lighting", _("Lighting")
 
     # Optional Services
-    SUMMER_STORAGE_FOR_DOCKING_EQUIPMENT = "summer_storage_for_docking_equipment"
-    SUMMER_STORAGE_FOR_TRAILERS = "summer_storage_for_trailers"
-    PARKING_PERMIT = "parking_permit"
-    DINGHY_PLACE = "dinghy_place"
-
-    class Labels:
-        ELECTRICITY = _("Electricity")
-        WATER = _("Water")
-        GATE = _("Gate")
-        MOORING = _("Mooring")
-        WASTE_COLLECTION = _("Waste collection")
-        LIGHTING = _("Lighting")
-        SUMMER_STORAGE_FOR_DOCKING_EQUIPMENT = _("Summer storage for docking equipment")
-        SUMMER_STORAGE_FOR_TRAILERS = _("Summer storage for trailers")
-        PARKING_PERMIT = _("Parking permit")
-        DINGHY_PLACE = _("Dinghy place")
+    SUMMER_STORAGE_FOR_DOCKING_EQUIPMENT = (
+        "summer_storage_for_docking_equipment",
+        _("Summer storage for docking equipment"),
+    )
+    SUMMER_STORAGE_FOR_TRAILERS = (
+        "summer_storage_for_trailers",
+        _("Summer storage for trailers"),
+    )
+    PARKING_PERMIT = "parking_permit", _("Parking permit")
+    DINGHY_PLACE = "dinghy_place", _("Dinghy place")
 
     @staticmethod
     def FIXED_SERVICES():
@@ -49,50 +43,25 @@ class ProductServiceType(Enum):
             ProductServiceType.DINGHY_PLACE,
         ]
 
-    def is_fixed_service(self):
-        return self in self.FIXED_SERVICES()
 
-    def is_optional_service(self):
-        return self in self.OPTIONAL_SERVICES()
-
-
-class AdditionalProductType(Enum):
-    FIXED_SERVICE = "fixed_service"
-    OPTIONAL_SERVICE = "optional_service"
-
-    class Labels:
-        FIXED_SERVICE = _("Fixed service")
-        OPTIONAL_SERVICE = _("Optional service")
+class AdditionalProductType(TextChoices):
+    FIXED_SERVICE = "fixed_service", _("Fixed service")
+    OPTIONAL_SERVICE = "optional_service", _("Optional service")
 
 
-class PeriodType(Enum):
-    YEAR = "year"
-    SEASON = "season"
-    MONTH = "month"
-
-    class Labels:
-        YEAR = _("Year")
-        SEASON = _("Season")
-        MONTH = _("Month")
+class PeriodType(TextChoices):
+    YEAR = "year", _("Year")
+    SEASON = "season", _("Season")
+    MONTH = "month", _("Month")
 
 
-class PriceUnits(Enum):
-    AMOUNT = "amount"
-    PERCENTAGE = "percentage"
-
-    class Labels:
-        AMOUNT = _("Amount")
-        PERCENTAGE = _("Percentage")
+class PriceUnits(TextChoices):
+    AMOUNT = "amount", _("Amount")
+    PERCENTAGE = "percentage", _("Percentage")
 
 
-class OrderStatus(Enum):
-    WAITING = "waiting"
-    REJECTED = "rejected"
-    EXPIRED = "expired"
-    PAID = "paid"
-
-    class Labels:
-        WAITING = _("Waiting")
-        REJECTED = _("Rejected")
-        EXPIRED = _("Expired")
-        PAID = _("Paid")
+class OrderStatus(TextChoices):
+    WAITING = "waiting", _("Waiting")
+    REJECTED = "rejected", _("Rejected")
+    EXPIRED = "expired", _("Expired")
+    PAID = "paid", _("Paid")
