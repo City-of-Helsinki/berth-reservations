ebf5595f5a95f6cdc50a09913f65977a4ca8b970
Santtu Laukkanen <santtu.laukkanen@gmail.com>
2020-09-08 11:09:04 +0300

    Send different email notification for umarked ws application

--- applications/tests/test_application_models.py
+++ applications/tests/test_application_models.py
@@ -1,7 +1,10 @@
 import pytest
 from django.core.exceptions import ValidationError
 
+from harbors.tests.factories import WinterStorageAreaFactory
+
 from ..enums import ApplicationStatus
+from .factories import WinterAreaChoiceFactory
 
 
 def test_berth_application_without_lease_valid_statuses(berth_application):
@@ -25,3 +28,25 @@ def test_berth_application_without_lease_invalid_statuses(berth_application):
     error_msg = str(exception.value)
     assert new_status.name in error_msg
     assert "BerthApplication with no lease can only be" in error_msg
+
+
+def test_winterstorage_application_is_not_unmarked(winter_storage_application):
+    area = WinterStorageAreaFactory()
+    area.number_of_unmarked_spaces = None
+    area.save()
+    WinterAreaChoiceFactory(
+        application=winter_storage_application, winter_storage_area=area
+    )
+
+    assert winter_storage_application.is_unmarked_ws_application() is False
+
+
+def test_unmarked_winterstorage_application_is_unmarked(winter_storage_application):
+    area = WinterStorageAreaFactory()
+    area.number_of_unmarked_spaces = 50
+    area.save()
+    WinterAreaChoiceFactory(
+        application=winter_storage_application, winter_storage_area=area
+    )
+
+    assert winter_storage_application.is_unmarked_ws_application() is True
