dc29c1d972d258f5d9fab7230afb5c5e843cf657
Teppo Nurminen <teppo@rakettitiede.com>
2020-10-15 22:21:18 +0300

    Run server with uwsgi in docker, fix docker image creation a bit, add createsuperuser command for use in review environments, add back db initialization script

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -19,6 +19,17 @@ if [[ "$APPLY_MIGRATIONS" = "1" ]]; then
 fi
 
 
-# Start server TODO run with uwsgi like tunnistamo?
-python ./manage.py runserver 0.0.0.0:8000
+if [[ "$CREATE_SUPERUSER" = "1" ]]; then
+python ./manage.py shell -c "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.filter(email='admin@example.com').delete(); User.objects.create_superuser('admin', 'admin@example.com', 'adminpass')"
+ echo "Admin user created with credentials admin:adminpass (email: admin@example.com)"
+fi
 
+# Start server
+if [[ ! -z "$@" ]]; then
+    "$@"
+elif [[ "$DEV_SERVER" = "1" ]]; then
+    python ./manage.py runserver 0.0.0.0:8000
+else
+    python ./manage.py collectstatic --noinput
+    uwsgi --ini .prod/uwsgi.ini
+fi
