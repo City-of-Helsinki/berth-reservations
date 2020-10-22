57f5bca08d1bfabf0c643a7a5e4e3b4d899a5e2e
Santtu Laukkanen <santtu.laukkanen@gmail.com>
2020-10-22 14:56:52 +0300

    Create command for assigning stickers for existing paid WS leases

--- .prod/on_deploy.sh
+++ .prod/on_deploy.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 # Runs migration scripts on each deployment
-exec python /app/manage.py migrate --noinput
+python /app/manage.py migrate --noinput
 
+# This can be removed once executed
+python /app/manage.py assign_ws_lease_stickers
