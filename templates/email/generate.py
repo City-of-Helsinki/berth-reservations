43a8373568bf2fc5eb6588fd8ca56d9f4e9c677c
Tatu Arvela <tatu.arvela@nitor.com>
2020-12-01 13:08:40 +0200

    Run linters on generate.py

--- templates/email/generate.py
+++ templates/email/generate.py
@@ -1,43 +1,42 @@
 import os
 import re
 
-
-messages_path = os.path.join(os.getcwd(), 'messages')
-generated_path = os.path.join(os.getcwd(), 'generated')
-template_file_extension = '.html'
+messages_path = os.path.join(os.getcwd(), "messages")
+generated_path = os.path.join(os.getcwd(), "generated")
+template_file_extension = ".html"
 
 
 def get_template_for_message(filename):
-    lang = filename.replace(template_file_extension, '')[-2:]
-    if lang != 'fi' and lang != 'sv' and lang != 'en':
-        lang = 'fi'
-    template_filename = 'base_template_' + lang + template_file_extension
+    lang = filename.replace(template_file_extension, "")[-2:]
+    if lang != "fi" and lang != "sv" and lang != "en":
+        lang = "fi"
+    template_filename = "base_template_" + lang + template_file_extension
     template_path = os.path.join(os.getcwd(), template_filename)
-    return open(template_path, 'r').read()
+    return open(template_path, "r").read()
 
 
 def get_content_tag_line(template):
-    return re.search('^.*{{ content }}.*$', template, re.MULTILINE).group(0)
+    return re.search("^.*{{ content }}.*$", template, re.MULTILINE).group(0)
 
 
 def get_indentation(line):
-    return len(line.split('{', 1)[0])
+    return len(line.split("{", 1)[0])
 
 
 def indent_content(content, indentation):
     content_lines = content.splitlines()
-    indented_content = ''
+    indented_content = ""
     for line in content_lines:
         if len(line) > 0:
-            indented_content = indented_content + indentation * ' ' + line + '\n'
+            indented_content = indented_content + indentation * " " + line + "\n"
         # if line is empty, don't indent
         else:
-            indented_content = indented_content + '\n'
+            indented_content = indented_content + "\n"
     return indented_content
 
 
 def generate_template(filename):
-    message = open(os.path.join(messages_path, filename), 'r').read()
+    message = open(os.path.join(messages_path, filename), "r").read()
 
     template = get_template_for_message(filename)
     content_tag_line = get_content_tag_line(template)
@@ -54,13 +53,15 @@ def create_generated_folder():
 
 
 def clear_generated_templates():
-    file_list = [f for f in os.listdir(generated_path) if f.endswith(template_file_extension)]
+    file_list = [
+        f for f in os.listdir(generated_path) if f.endswith(template_file_extension)
+    ]
     for f in file_list:
         os.remove(os.path.join(generated_path, f))
 
 
 def save_template(filename, content):
-    with open(os.path.join(generated_path, filename), 'x') as f:
+    with open(os.path.join(generated_path, filename), "x") as f:
         f.write(content)
 
 
