79d8121c5cb78c9dbaaa6477d5bfd35e16ccfd6b
Alex Ramirez <armzprz@gmail.com>
2021-01-14 11:26:39 +0200

    Add django-silk for profiling

--- berth_reservations/urls.py
+++ berth_reservations/urls.py
@@ -1,4 +1,5 @@
 from django.conf import settings
+from django.conf.urls import url
 from django.conf.urls.static import static
 from django.http import HttpResponse
 from django.urls import include, path
@@ -21,6 +22,9 @@ urlpatterns = [
     ),
     path("payments/", include(payment_urls)),
 ]
+
+if settings.ENABLE_PROFILING_TOOLS:
+    urlpatterns += [url(r"^silk/", include("silk.urls", namespace="silk"))]
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
