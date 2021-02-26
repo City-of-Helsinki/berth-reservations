eb8368ed675547c4bcf2677124d8b66764a97123
Alex Ramirez <armzprz@gmail.com>
2021-02-26 15:16:01 +0200

    Allow error leases to be active
    Since error leases should still go but are pending to be fixed, their
    berths should not be released.

--- leases/consts.py
+++ leases/consts.py
@@ -1,4 +1,9 @@
 from .enums import LeaseStatus
 
-ACTIVE_LEASE_STATUSES = (LeaseStatus.DRAFTED, LeaseStatus.OFFERED, LeaseStatus.PAID)
+ACTIVE_LEASE_STATUSES = (
+    LeaseStatus.DRAFTED,
+    LeaseStatus.OFFERED,
+    LeaseStatus.PAID,
+    LeaseStatus.ERROR,
+)
 INACTIVE_LEASE_STATUSES = (LeaseStatus.REFUSED, LeaseStatus.EXPIRED)
