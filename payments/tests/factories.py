6cc73fdb668120ea0b3a061e23345f2664c150e2
Alex Ramirez <armzprz@gmail.com>
2021-03-15 10:08:53 +0200

    Fix switch offer factory to always have a paid lease

--- payments/tests/factories.py
+++ payments/tests/factories.py
@@ -6,6 +6,7 @@ from dateutil.utils import today
 
 from applications.tests.factories import BerthApplicationFactory, BerthSwitchFactory
 from berth_reservations.tests.factories import CustomerProfileFactory
+from leases.enums import LeaseStatus
 from leases.tests.factories import BerthLeaseFactory, WinterStorageLeaseFactory
 from leases.utils import calculate_season_end_date, calculate_season_start_date
 from resources.tests.factories import BerthFactory, WinterStorageAreaFactory
@@ -197,6 +198,7 @@ class BerthSwitchOfferFactory(AbstractOfferFactory):
     lease = factory.SubFactory(
         BerthLeaseFactory,
         customer=factory.SelfAttribute("..customer"),
+        status=LeaseStatus.PAID,
         start_date=calculate_season_start_date(today()) - relativedelta(years=1),
         end_date=calculate_season_end_date(today()) - relativedelta(years=1),
     )
