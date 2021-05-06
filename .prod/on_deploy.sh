ba1e5774ab73ca36068b35c4ae69fddff742165b
Matti Haavikko <haavikko@gmail.com>
2021-05-06 07:02:56 +0300

    VEN-1245: add "Berth customer" group

--- .prod/on_deploy.sh
+++ .prod/on_deploy.sh
@@ -3,5 +3,8 @@
 # Runs migration scripts on each deployment
 python /app/manage.py migrate --noinput
 
+# Berth customers group added 2021-05, this line can be removed after prod deployment done successfully
+python /app/manage.py loaddata groups.json
+
 # Updates group permissions on each deployment in case of updated permissions
 python /app/manage.py set_group_model_permissions
--- users/utils.py
+++ users/utils.py
@@ -1,3 +1,10 @@
+from functools import lru_cache
+
+from django.conf import settings
+from django.contrib.auth.models import Group
+from django.utils.translation import gettext_lazy as _
+from graphql_jwt.exceptions import PermissionDenied
+
 VALID_PERMS = ["view", "add", "change", "delete"]
 
 
@@ -37,3 +44,47 @@ def user_has_change_permission(user, *models):
 
 def user_has_delete_permission(user, *models):
     return _user_has_models_perms("delete", models)(user)
+
+
+def is_customer(user):
+    if user.is_staff or user.is_superuser:
+        return False
+    elif (
+        user.groups.count() == 1
+        and user.groups.first().name == settings.CUSTOMER_GROUP_NAME
+    ):
+        return True
+    return False
+
+
+@lru_cache(maxsize=3)
+def get_berth_customers_group():
+    return Group.objects.get(name=settings.CUSTOMER_GROUP_NAME)
+
+
+def get_node_user(node):
+    """
+       Try to get to the user this node belongs to
+       either directly by node.user or through CustomerProfile,
+       i.e. by doing node.customer.user
+    """
+    if not node:
+        return None
+
+    node_user = None
+    if hasattr(node, "user"):
+        node_user = node.user
+    elif hasattr(node, "customer") and hasattr(node.customer, "user"):
+        node_user = node.customer.user
+    return node_user
+
+
+def user_is_linked_to_node(user, node):
+    if user is None:
+        return False
+    return user == get_node_user(node)
+
+
+def validate_user_is_linked_to_node(user, node):
+    if not user_is_linked_to_node(user, node):
+        raise PermissionDenied(_("You do not have permission to perform this action."))
