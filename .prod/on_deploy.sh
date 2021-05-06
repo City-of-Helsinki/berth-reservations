2cc09bd387a1c005b1c7f3e5d377dad1fd795646
Alex Ramirez <armzprz@gmail.com>
2021-05-06 15:13:30 +0300

    Re-enable deploy migrations and permission setting

--- .prod/on_deploy.sh
+++ .prod/on_deploy.sh
@@ -1,10 +1,7 @@
 #!/bin/sh
 
 # Runs migration scripts on each deployment
-# TODO: Temporarily disable the automatic migrations since, on this case,
-#  some commands need to be executed before the migration,
-#  and those commands will be executed manually ONCE
-# python /app/manage.py migrate --noinput
+ python /app/manage.py migrate --noinput
 
 # Updates group permissions on each deployment in case of updated permissions
-# python /app/manage.py set_group_model_permissions
+ python /app/manage.py set_group_model_permissions
