b56da30cd1f969d1a4ab920849922980ce70255e
Matti Haavikko <haavikko@gmail.com>
2021-05-07 13:27:03 +0300

    Revert "Disable temporarily the commands on deploy"

--- .prod/on_deploy.sh
+++ .prod/on_deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Runs migration scripts on each deployment
-# python /app/manage.py migrate --noinput
+ python /app/manage.py migrate --noinput
 
 # Updates group permissions on each deployment in case of updated permissions
-# python /app/manage.py set_group_model_permissions
+ python /app/manage.py set_group_model_permissions
